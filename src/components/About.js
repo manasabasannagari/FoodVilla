b432ff2657a203e2917d147d1defe3c8c4bc7d4f
Manasa Basannagari <Manasa.Basannagari1@T-Mobile.com>
2023-08-02 11:02:59 +0530

    changes added

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Profile from './ProfileClass.js'
-const AboutUs2 = () => {
+const AboutUs = () => {
     return (
         <>
         <div>
@@ -18,22 +18,22 @@ const AboutUs2 = () => {
     )
 }
 
-class AboutUs extends React.Component {
-    render(){
-        return (
-            <>
-            <div>
-                <h1>This is About Us Page</h1>
-                <p>Food Villa is for feeding lots of hungry people over the world....</p>
-                <Link to="profile">
-                    Profile Page
-                </Link> 
-                <Outlet/>
-                <Profile name="Manasa"/>
-            </div>
-            </>
-        )
-    }
-}
+// class AboutUs extends React.Component {
+//     render(){
+//         return (
+//             <>
+//             <div>
+//                 <h1>This is About Us Page</h1>
+//                 <p>Food Villa is for feeding lots of hungry people over the world....</p>
+//                 <Link to="profile">
+//                     Profile Page
+//                 </Link> 
+//                 <Outlet/>
+//                 <Profile name="Manasa"/>
+//             </div>
+//             </>
+//         )
+//     }
+// }
 
 export default AboutUs;
